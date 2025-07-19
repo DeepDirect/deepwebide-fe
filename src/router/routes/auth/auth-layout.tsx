47075e7097c25474c52c74444b882a6066ee6d82
ssoogit <ssoogit02@gmail.com>
2025-07-19 23:57:48 +0900

    feat(router-auth-layout): 레이아웃의 경우 경로를 설정하지 않아 제거, id만 사용 / DP-30

--- src/router/routes/auth/auth-layout.tsx
+++ src/router/routes/auth/auth-layout.tsx
@@ -1,10 +1,9 @@
 import { createRoute } from '@tanstack/react-router';
-import { rootRoute } from '../../root';
+import { rootRoute } from '@/router/root';
 import AuthLayout from '@/layouts/AuthLayout/AuthLayout';
 
 export const authLayoutRoute = createRoute({
   getParentRoute: () => rootRoute,
   id: 'auth-layout',
-  path: '/',
   component: AuthLayout,
 });
