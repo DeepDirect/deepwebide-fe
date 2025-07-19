ee45d26e9582dec41319e87ffc00e0ef6f7a42d7
ssoogit <ssoogit02@gmail.com>
2025-07-19 21:54:47 +0900

    feat(authLayout): tanstack router 사용을 위해 outlet으로 변경 / DP-30

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,16 +1,14 @@
-import type { ReactNode } from 'react';
+import { Outlet } from '@tanstack/react-router';
 import styles from './AuthLayout.module.scss';
 import Header from '@/components/Header/Header';
 
-interface Props {
-  children: ReactNode;
-}
-
-const AuthLayout = ({ children }: Props) => {
+const AuthLayout = () => {
   return (
     <div className={styles.container}>
       <Header variant="auth" />
-      <main className={styles.content}>{children}</main>
+      <main className={styles.content}>
+        <Outlet />
+      </main>
     </div>
   );
 };
