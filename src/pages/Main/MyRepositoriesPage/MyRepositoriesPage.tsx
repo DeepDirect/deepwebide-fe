c6c9a86428f71b6d71df0ba3261e8bb02287f8d8
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-20 09:34:54 +0900

    feat: add MyRepositoriesPage file / DP-40

---
+++ src/pages/Main/MyRepositoriesPage/MyRepositoriesPage.tsx
@@ -0,0 +1,11 @@
+import styles from './MyRepositoriesPage.module.scss';
+
+const MyRepositoriesPage = () => {
+  return (
+    <>
+      <h1 className={styles.title}></h1>
+    </>
+  );
+};
+
+export default MyRepositoriesPage;
