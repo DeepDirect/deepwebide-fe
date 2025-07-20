9ed8fd97f9bed70f6776c9deabb6c391f9728602
ssoogit <ssoogit02@gmail.com>
2025-07-20 23:33:35 +0900

    feat(sign-in): 별도로 분리한 폼과 연결 / DP-30

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,8 +1,6 @@
-import PasswordInput from '@/components/atoms/Input/PasswordInput';
-import styles from './SignInPage.module.scss';
-import Input from '@/components/atoms/Input/Input';
-import Button from '@/components/atoms/Button/Button';
+import SignInForm from './SignInForm';
 import { Link } from '@tanstack/react-router';
+import styles from './SignInPage.module.scss';
 
 export default function SignInPage() {
   return (
@@ -10,29 +8,7 @@ export default function SignInPage() {
       <h1 className={styles.title}>로그인</h1>
       <p className={styles.text}>이메일과 비밀번호를 입력하고 서비스를 이용하세요!</p>
 
-      <div className={styles.formGroup}>
-        <h3 className={styles.subTitle}>
-          이메일<span className={styles.required}>*</span>
-        </h3>
-        <Input name="email" placeholder="user@goorm.com" />
-      </div>
-      <div className={styles.formGroup}>
-        <h3 className={styles.subTitle}>
-          비밀번호<span className={styles.required}>*</span>
-        </h3>
-        <PasswordInput name="password" placeholder="********" />
-      </div>
-
-      <div className={styles.options}>
-        <label className={styles.checkbox}>
-          <input type="checkbox" />
-          <p>이메일 저장하기</p>
-        </label>
-      </div>
-
-      <Button variant="active" className={styles.loginBtn}>
-        로그인
-      </Button>
+      <SignInForm />
 
       <div className={styles.links}>
         <Link to="/" className={styles.link}>
