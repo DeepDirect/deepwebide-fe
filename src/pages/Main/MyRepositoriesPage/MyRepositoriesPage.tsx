87cb40a047a3578e357c4eb46923e1cfd20a60e6
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-20 16:02:56 +0900

    feat(main): 레포지토리 리스트 아이템 추가 / DP-40

--- src/pages/Main/MyRepositoriesPage/MyRepositoriesPage.tsx
+++ src/pages/Main/MyRepositoriesPage/MyRepositoriesPage.tsx
@@ -2,6 +2,7 @@ import styles from './MyRepositoriesPage.module.scss';
 
 import Button from '@/components/atoms/Button/Button';
 import FileIcon from '@/assets/icons/file.svg?react';
+import Repository from '@/components/Repository/Repository';
 import Toggle from '@/components/atoms/Toggle/Toggle';
 
 const MyRepositoriesPage = () => {
@@ -16,6 +17,16 @@ const MyRepositoriesPage = () => {
             <FileIcon className={styles.iconImage} />새 레포지토리 생성
           </Button>
         </div>
+      </div>
+
+      <div className={styles.repositoriesWrapper}>
+        <Repository />
+        <Repository />
+        <Repository />
+        <Repository />
+        <Repository />
+        <Repository />
+        <Repository />
       </div>
     </div>
   );
