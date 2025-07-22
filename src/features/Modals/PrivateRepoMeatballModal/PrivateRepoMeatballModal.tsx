6cc4a0dfbc191bfa1756c293df05a44a8e26b7b0
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-07-22 16:47:21 +0900

    feat: PrivateRepoMeatball 생성 / DP-54

---
+++ src/features/Modals/PrivateRepoMeatballModal/PrivateRepoMeatballModal.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import MenuItem from '@/components/molecules/MenuItem/MenuItem';
+import MeatballModal from '@/components/organisms/Modals/MeatballModal/MeatballModal';
+
+export interface PrivateRepoMeatballModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  position?: { top: number; left: number };
+  onRename?: () => void;
+  onShare?: () => void;
+  onDelete?: () => void;
+  className?: string;
+}
+
+const PrivateRepoMeatballModal: React.FC<PrivateRepoMeatballModalProps> = ({
+  open,
+  onOpenChange,
+  position = { top: 0, left: 0 },
+  onRename,
+  onShare,
+  onDelete,
+  className = '',
+}) => {
+  const handleRename = () => {
+    if (onRename) {
+      onRename();
+    }
+    onOpenChange(false);
+  };
+
+  const handleShare = () => {
+    if (onShare) {
+      onShare();
+    }
+    onOpenChange(false);
+  };
+
+  const handleDelete = () => {
+    if (onDelete) {
+      onDelete();
+    }
+    onOpenChange(false);
+  };
+
+  return (
+    <MeatballModal
+      open={open}
+      onOpenChange={onOpenChange}
+      position={position}
+      className={className}
+    >
+      <MenuItem
+        label="이름 변경하기"
+        iconPath="/src/assets/icons/edit-box.svg"
+        onClick={handleRename}
+        variant="default"
+      />
+
+      <MenuItem
+        label="공유하기"
+        iconPath="/src/assets/icons/forward.svg"
+        onClick={handleShare}
+        variant="default"
+      />
+
+      <MenuItem
+        label="삭제하기"
+        iconPath="/src/assets/icons/trash.svg"
+        onClick={handleDelete}
+        variant="red"
+      />
+    </MeatballModal>
+  );
+};
+
+export default PrivateRepoMeatballModal;
