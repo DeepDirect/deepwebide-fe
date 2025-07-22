2daa371d521b5baf404bbcc97918de9a41660c45
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-22 10:47:17 +0900

    feat(route): 공유받은 레포 페이지 라우터에 추가 / DP-52

--- src/router/routeTree.ts
+++ src/router/routeTree.ts
@@ -5,8 +5,13 @@ import { signUpRoute } from './routes/auth/sign-up';
 import { mainLayoutRoute } from './routes/main/main-layout';
 import { myRepositoriesRoute } from './routes/main/my-repositories';
 import { SharedRepositoriesRoute } from './routes/main/shared-repositories';
+import { SharedMeRepositoriesRoute } from './routes/main/shared-me-repositories';
 
 export const routeTree = rootRoute.addChildren([
   authLayoutRoute.addChildren([signInRoute, signUpRoute]),
-  mainLayoutRoute.addChildren([myRepositoriesRoute, SharedRepositoriesRoute]),
+  mainLayoutRoute.addChildren([
+    myRepositoriesRoute,
+    SharedRepositoriesRoute,
+    SharedMeRepositoriesRoute,
+  ]),
 ]);
