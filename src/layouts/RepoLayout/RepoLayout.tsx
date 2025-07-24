0841c95ba6c4ea3f34e33afcd889145f7cb7e1ea
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-07-24 09:27:16 +0900

    feat: RepoLayout 생성 / DP-96

---
+++ src/layouts/RepoLayout/RepoLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from '@tanstack/react-router';
+import styles from './RepoLayout.module.scss';
+import clsx from 'clsx';
+
+import Header from '@/components/organisms/Header/Header';
+import { Sidebar } from '@/components/organisms/Sidebar/RepoSidebar/RepoSidebar';
+import { useThemeStore } from '@/stores/themeStore';
+
+export function RepoLayout() {
+  const { isDarkMode } = useThemeStore();
+
+  return (
+    <div className={clsx(styles.RepoLayout, { [styles.RepoLayoutDark]: isDarkMode })}>
+      <Header variant="repo" />
+      <Sidebar />
+      <main className="content-area">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
