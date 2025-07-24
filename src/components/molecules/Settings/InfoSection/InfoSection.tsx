c0fd33fd5772483bb9f33eab377e5f5905c31d4d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-24 20:11:17 +0900

    feat: add InfoSection component / DP-105

---
+++ src/components/molecules/Settings/InfoSection/InfoSection.tsx
@@ -0,0 +1,46 @@
+import 'dayjs/locale/ko';
+
+import dayjs from 'dayjs';
+
+import styles from './InfoSection.module.scss';
+
+import InfoIcon from '@/assets/icons/info.svg?react';
+import EditIcon from '@/assets/icons/edit-box.svg?react';
+
+type infoSectionProps = {
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+const InfoSection = ({ name, createdAt, updatedAt }: infoSectionProps) => {
+  return (
+    <section className={styles.infoSection}>
+      <div className={styles.sectionTitleWrapper}>
+        <InfoIcon className={styles.nameIcon} />
+        <h2 className={styles.sectionTitle}>INFO</h2>
+      </div>
+      <div className={styles.grid}>
+        <div className={styles.label}>NAME</div>
+        <div className={`${styles.value} ${styles.nameWrapper}`}>
+          <span>{name}</span>
+          <button className={styles.editButton}>
+            <EditIcon className={styles.icon} />
+          </button>
+        </div>
+
+        <div className={styles.label}>CREATED AT</div>
+        <div className={styles.value}>
+          {dayjs(createdAt).locale('ko').format('YYYY-MM-DD HH:MM:ss')}
+        </div>
+
+        <div className={styles.label}>UPDATED AT</div>
+        <div className={styles.value}>
+          {dayjs(updatedAt).locale('ko').format('YYYY-MM-DD HH:MM:ss')}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default InfoSection;
