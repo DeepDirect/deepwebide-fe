e2c3410bfe8cb999f2f65c69777cf030fa462933
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-25 17:49:11 +0900

    fix: 로그인 여부에 따라 로그아웃 처리 되도록 수정 / DP-114

--- src/router/routes/main/main-layout.tsx
+++ src/router/routes/main/main-layout.tsx
@@ -1,11 +1,22 @@
 import { createRoute, redirect } from '@tanstack/react-router';
 import { rootRoute } from '@/router/root';
 import MainLayout from '@/layouts/MainLayout/MainLayout';
+import type { AuthState } from '@/types/authState.types';
 
 export const mainLayoutRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: 'main',
   component: MainLayout,
+  beforeLoad: ctx => {
+    const { auth } = ctx.context as { auth: AuthState };
+
+    if (!auth.isLoggedIn) {
+      throw redirect({
+        to: '/sign-in',
+        search: { redirect: location.href },
+      });
+    }
+  },
 });
 
 export const mainIndexRoute = createRoute({
