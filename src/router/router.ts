9bcfc11ac92cf613645603c514aabb14e7f1f475
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-25 17:48:38 +0900

    fix: 라우터에 context 추가 / DP-114

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,10 @@
 import { createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree';
+import type { AuthState } from '@/types/authState.types';
 
 export const router = createRouter({
   routeTree,
+  context: {
+    auth: undefined as unknown as AuthState,
+  },
 });
