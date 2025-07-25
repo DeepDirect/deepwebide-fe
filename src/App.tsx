8c6a234dcce86f3b7b0711b21aef31414c99d7df
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-25 17:58:41 +0900

    fix: auth store 코드 제거 / DP-114

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router/router';
-import { useAuthStore } from './stores/authStore';
 
 function App() {
-  const auth = useAuthStore();
-
-  return <RouterProvider router={router} context={{ auth }} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
