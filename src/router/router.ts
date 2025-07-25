76102b6c458aaf9ab52e8f7b2fb1410a24bf6799
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-25 17:57:56 +0900

    fix: Router context에 auth store 추가 / DP-114

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,11 @@
 import { createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree';
-import type { AuthState } from '@/types/authState.types';
+import { useAuthStore } from '@/stores/authStore';
 
+const auth = useAuthStore.getState();
 export const router = createRouter({
   routeTree,
   context: {
-    auth: undefined as unknown as AuthState,
+    auth,
   },
 });
