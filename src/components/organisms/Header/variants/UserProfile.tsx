3ac55cdb2b6965929860c9c75608db2f0ce98a6b
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-25 17:57:11 +0900

    fix: 로그아웃 처리 후 로그인 페이지로 이동 / DP-114

--- src/components/organisms/Header/variants/UserProfile.tsx
+++ src/components/organisms/Header/variants/UserProfile.tsx
@@ -2,14 +2,17 @@ import styles from './HeaderVariant.module.scss';
 import ProfileDropdown from '@/components/molecules/Modals/ProfileDropdown/ProfileDropdown';
 import moodHappyIcon from '@/assets/icons/mood-happy.svg';
 import { useAuthStore } from '@/stores/authStore';
+import { useNavigate } from '@tanstack/react-router';
 
 const UserProfile = () => {
   const { signout } = useAuthStore();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     // TODO: 로그아웃 처리 로직 구현
     console.log('로그아웃 처리');
     signout();
+    navigate({ to: '/sign-in' });
   };
 
   return (
