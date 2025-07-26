d90debe461b109c6b3f2341f1fd39356285381cb
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-27 01:05:29 +0900

    fix: CreateRepoRequestSchema의 repositoryType 문자열에서 enum으로 타입 수정 / DP-112

--- src/schemas/repo.schema.ts
+++ src/schemas/repo.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const CreateRepoRequestSchema = z.object({
   repositoryName: z.string(),
-  repositoryType: z.string(),
+  repositoryType: z.enum({ SPRING_BOOT: 'SPRING_BOOT', REACT: 'REACT', FAST_API: 'FAST_API' }),
 });
 
 export const CreateRepoResponseSchema = z.object({
