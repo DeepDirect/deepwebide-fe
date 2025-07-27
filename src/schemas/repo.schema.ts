af01f233814bf04731e4afb47bd8572565bda2cc
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-28 00:27:33 +0900

    fix: 레포 공유 설정 response 타입 추가 / DP-112

--- src/schemas/repo.schema.ts
+++ src/schemas/repo.schema.ts
@@ -26,7 +26,22 @@ export const RepositoryRenameResponseSchema = z.object({
   updatedAt: z.string(),
 });
 
+export const UpdateRepositoryShareStatusResponseSchema = z.object({
+  repositoryId: z.number(),
+  repositoryName: z.string(),
+  ownerId: z.number(),
+  ownerName: z.string(),
+  shareLink: z.string().url(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  isFavorite: z.boolean(),
+  isShared: z.boolean(),
+});
+
 export type CreateRepoRequest = z.infer<typeof CreateRepoRequestSchema>;
 export type CreateRepoResponse = z.infer<typeof CreateRepoResponseSchema>;
 export type RepositoryRenameRequest = z.infer<typeof RepositoryRenameRequestSchema>;
 export type RepositoryRenameResponse = z.infer<typeof RepositoryRenameResponseSchema>;
+export type UpdateRepositoryShareStatusResponse = z.infer<
+  typeof UpdateRepositoryShareStatusResponseSchema
+>;
