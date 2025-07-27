d549b4ef8fc083dddd22d3f491f82cd5cf05f384
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-28 01:52:06 +0900

    fix: 공유한 레포 페이지에 api 추가 / DP-112

--- src/pages/Main/SharedByMeRepoPage/SharedByMeRepoPage.tsx
+++ src/pages/Main/SharedByMeRepoPage/SharedByMeRepoPage.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from '@tanstack/react-router';
+
+import useGetRepository from '@/hooks/useGetRepository';
 
 import Toggle from '@/components/atoms/Toggle/Toggle';
 import Pagination from '@/components/molecules/Pagination/Pagination';
@@ -7,102 +9,64 @@ import RepoListItem from '@/components/organisms/RepoListItem/RepoListItem';
 
 import MainPageType from '@/constants/enums/MainPageType.enum';
 
+import type { RepositoryItem } from '@/schemas/main.schema';
+import type { RepositoryQueryURL } from '@/types/apiEndpoints.types';
+import type { Page } from '@/types/page.types';
+
 import styles from './SharedByMeRepoPage.module.scss';
 
-const tempList = [
-  {
-    repositoryId: 1,
-    repositoryName: '공유한 프로젝트1',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-18T13:10:00Z',
-    updatedAt: '2025-07-22T13:10:00Z',
-    isFavorite: true,
-  },
-  {
-    repositoryId: 2,
-    repositoryName: '공유한 프로젝트2',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-18T13:10:00Z',
-    updatedAt: '2025-07-22T13:10:00Z',
-    isFavorite: false,
-  },
-  {
-    repositoryId: 3,
-    repositoryName: '공유한 프로젝트3',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-19T08:00:00Z',
-    updatedAt: '2025-07-22T13:20:00Z',
-    isFavorite: true,
-  },
-  {
-    repositoryId: 4,
-    repositoryName: '공유한 프로젝트4',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-19T08:00:00Z',
-    updatedAt: '2025-07-22T13:20:00Z',
-    isFavorite: false,
-  },
-  {
-    repositoryId: 5,
-    repositoryName: '공유한 프로젝트5',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-20T11:00:00Z',
-    updatedAt: '2025-07-22T14:00:00Z',
-    isFavorite: true,
-  },
-  {
-    repositoryId: 6,
-    repositoryName: '공유한 프로젝트6',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-20T11:00:00Z',
-    updatedAt: '2025-07-22T14:00:00Z',
-    isFavorite: false,
-  },
-  {
-    repositoryId: 7,
-    repositoryName: '공유한 프로젝트7',
-    ownerId: 5,
-    ownerName: '슬기로운 개발자',
-    isShared: false,
-    shareLink: null,
-    createdAt: '2025-07-20T14:30:00Z',
-    updatedAt: '2025-07-22T14:30:00Z',
-    isFavorite: false,
-  },
-]; // TODO: api 연동 후 제거
+const getRepoURL: RepositoryQueryURL = '/api/repositories/shared';
 
 const SharedByMeRepoPage = () => {
   const navigate = useNavigate();
-  const [pagination, setPagination] = useState({
-    total: 10,
-    current: 1,
-    pageSize: 5,
-  }); // TODO: api 연동 후 받은 데이터로 변경
+  const [pagination, setPagination] = useState<Page>({
+    maxVisiblePages: 5,
+    page: null, // 1부터 시작
+    current: 1, // 현재 페이지
+    size: 7, // 아이템 개수
+    total: null, // 총 페이지 수
+  });
+  const [repositories, setRepositories] = useState<RepositoryItem[] | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+  const {
+    data,
+    isSuccess,
+    refetch,
+    // isLoading
+  } = useGetRepository(getRepoURL, {
+    page: (pagination.page || 1) - 1,
+    size: pagination.current || 7,
+    liked: isLiked,
+  });
 
+  // 성공
+  useEffect(() => {
+    if (isSuccess && data) {
+      setRepositories(data.repositories);
+      setPagination(prev => ({
+        ...prev,
+        total: data.totalPages,
+      }));
+    }
+  }, [isSuccess, data]);
+
+  // 페이지
   const handlePageChange = (page: number) => {
     setPagination(prev => ({ ...prev, current: page }));
+    refetch();
   };
+
+  // 레포 좋아요
   const handleFavoriteClick = (id: number) => {
     console.log(`Favorite clicked for repository ID: ${id}`);
   };
+
+  // 좋아요 필터
+  const handleLikChange = () => {
+    setIsLiked(!isLiked);
+    refetch();
+  };
+
   const handleRepoClick = (repoId: number) => {
     navigate({ to: '/$repoId', params: { repoId } });
   };
@@ -113,16 +77,16 @@ const SharedByMeRepoPage = () => {
         <h1 className={styles.title}>공유한 레포</h1>
 
         <div className={styles.buttonWrapper}>
-          <Toggle variant="favorite" />
+          <Toggle variant="favorite" onCheckedChange={handleLikChange} />
         </div>
       </div>
 
       <div className={styles.repositoriesWrapper}>
-        {tempList.map(repo => (
+        {repositories?.map(repo => (
           <RepoListItem
             key={repo.repositoryId}
             info={repo}
-            pageType={MainPageType.SHARED_BY_ME}
+            pageType={MainPageType.PRIVATE_REPO}
             handleFavoriteClick={handleFavoriteClick}
             handleRepoClick={handleRepoClick}
           />
@@ -131,9 +95,9 @@ const SharedByMeRepoPage = () => {
 
       <div className={styles.paginationWrapper}>
         <Pagination
-          maxVisiblePages={5}
-          totalPages={pagination.total}
-          currentPage={pagination.current}
+          maxVisiblePages={pagination.total || 1}
+          totalPages={pagination.total || 1}
+          currentPage={pagination.current || 1}
           handlePageChange={handlePageChange}
         />
       </div>
