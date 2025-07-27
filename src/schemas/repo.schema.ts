3e603bdaedec396ef36739b9eeabdda8cb0ed9d9
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-28 02:04:51 +0900

    fix: RepositoryEntrycodeResponse 타입 추가 / DP-112

--- src/schemas/repo.schema.ts
+++ src/schemas/repo.schema.ts
@@ -38,6 +38,18 @@ export const UpdateRepositoryShareStatusResponseSchema = z.object({
   isShared: z.boolean(),
 });
 
+export const RepositoryEntrycodeResponseSchema = z.object({
+  repositoryId: z.number(),
+  repositoryName: z.string(),
+  ownerId: z.number(),
+  ownerName: z.string(),
+  shareLink: z.string(),
+  entryCode: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  isShared: z.boolean(),
+});
+
 export type CreateRepoRequest = z.infer<typeof CreateRepoRequestSchema>;
 export type CreateRepoResponse = z.infer<typeof CreateRepoResponseSchema>;
 export type RepositoryRenameRequest = z.infer<typeof RepositoryRenameRequestSchema>;
@@ -45,3 +57,4 @@ export type RepositoryRenameResponse = z.infer<typeof RepositoryRenameResponseSc
 export type UpdateRepositoryShareStatusResponse = z.infer<
   typeof UpdateRepositoryShareStatusResponseSchema
 >;
+export type RepositoryEntrycodeResponse = z.infer<typeof RepositoryEntrycodeResponseSchema>;
