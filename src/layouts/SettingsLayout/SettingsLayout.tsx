717075b1b527def76df07a96601b86ee50b4ffca
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-28 14:42:31 +0900

    fix: 환경설정 레이아웃에서 사이드바 제거 / DP-138

--- src/layouts/SettingsLayout/SettingsLayout.tsx
+++ src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -2,7 +2,6 @@ import styles from './SettingsLayout.module.scss';
 
 import { Outlet } from '@tanstack/react-router';
 
-import { Sidebar } from '@/components/organisms/Sidebar/RepoSidebar/RepoSidebar';
 import MainHeader from '@/components/organisms/Header/MainHeader/MainHeader';
 
 const SettingsLayout = () => {
@@ -14,10 +13,6 @@ const SettingsLayout = () => {
       <header className={styles.header}>
         <MainHeader />
       </header>
-
-      <aside className={styles.sidebar}>
-        <Sidebar />
-      </aside>
 
       <main className={styles.contentArea}>
         <Outlet />
