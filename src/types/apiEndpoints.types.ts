af4cd1a6e4a5077aa8618231420b1cf22a6896e1
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-28 10:15:16 +0900

    fix: 레포 나가기 endpoint 추가 / DP-112

--- src/types/apiEndpoints.types.ts
+++ src/types/apiEndpoints.types.ts
@@ -8,3 +8,5 @@ export type CreateRepoURL = '/api/repositories';
 export type RepositoryURL = `/api/repositories/${number}`;
 
 export type RepositoryEntryCodeURL = `/api/repositories/${number}/entrycode`;
+
+export type RepositoryExitURL = `/api/repositories/${number}/exit`;
