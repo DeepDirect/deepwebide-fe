7932fc44ce63ae9161a9a8826696df3f1095cd36
eunlee <eklee2057@naver.com>
2025-07-28 14:34:27 +0900

    feat(InvitationLinkPage): 입장 코드 API 호출 및 성공 시 레포지토리로 이동 기능 추가 / DP-115

--- src/pages/Auth/InvitationLinkPage/InvitationLinkPage.tsx
+++ src/pages/Auth/InvitationLinkPage/InvitationLinkPage.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
+import { useNavigate, useParams, useSearch } from '@tanstack/react-router';
 import InvitationLinkForm from '@/components/organisms/InvitationLinkForm/InvitationLinkForm';
+import { entrycodeApi } from '@/api/entrycode.api';
 
 import './InvitationLinkPage.scss';
 
 const InvitationLinkPage: React.FC = () => {
-  const handleSubmit = (code: string) => {
-    console.log('입장 코드: ', code);
-    // TODO: 입장 로직 구현
+  const navigator = useNavigate();
+  const { repoId } = useParams({ strict: false });
+  const search = useSearch({ strict: false });
+  const repositoryName = search?.repositoryName;
+
+  const handleSubmit = async (code: string) => {
+    try {
+      // 입장 코드 API 호출
+      await entrycodeApi.postRepositoryEntryCode(repoId, code);
+
+      // 성공 시 레포지토리로 이동
+      // TODO - 메시지 토스트로 처리
+      alert('공유 레포지토리에 참여되었습니다.');
+
+      navigator({
+        to: '/$repoId',
+        params: { repoId },
+      });
+    } catch {
+      // TODO - 에러 메시지 토스트로 처리
+      alert('입장 코드 검증에 실패했습니다. 다시 시도해주세요.');
+    }
   };
 
   const handleGoHome = () => {
-    // TODO: 홈으로 이동 로직 구현
+    navigator({ to: '/main' });
   };
 
   return (
-    // TODO: 로그인 된 유저만 접근 가능하도록 구현
-
     <div className="invitation-link-page">
       <main className="invitation-link-page__content">
-        <InvitationLinkForm onSubmit={handleSubmit} onGoHome={handleGoHome} />
+        <InvitationLinkForm
+          onSubmit={handleSubmit}
+          onGoHome={handleGoHome}
+          repositoryName={repositoryName}
+        />
       </main>
-      {/* <Loading /> */}
     </div>
   );
 };
