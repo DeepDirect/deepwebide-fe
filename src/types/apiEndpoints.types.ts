1fd6bc78aefee09861e8f06eba3df36bace8f2b9
eunlee <eklee2057@naver.com>
2025-07-29 20:02:55 +0900

    feat(api): 레포 환경 설정 관련 API 엔드포인트 추가 / DP-124

--- src/types/apiEndpoints.types.ts
+++ src/types/apiEndpoints.types.ts
@@ -12,3 +12,7 @@ export type RepositoryEntryCodeURL = `/api/repositories/${number}/entrycode`;
 export type RepositoryExitURL = `/api/repositories/${number}/exit`;
 
 export type RepositoryFavoriteURL = `/api/repositories/${number}/favorite`;
+
+export type RepositorySettingsURL = `/api/repositories/${number}/settings`;
+
+export type KickedMemberURL = `/api/repositories/${number}/kicked`;
