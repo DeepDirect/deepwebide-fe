f84fbce888092a8961e130e40288c1e71290614d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-30 19:28:30 +0900

    fix: 토스트 스타일이 type에 따라 달라지도록 수정함 / DP-156

--- src/features/Toast/Toast.tsx
+++ src/features/Toast/Toast.tsx
@@ -3,11 +3,15 @@ import { useToastStore } from '@/stores/toastStore';
 import styles from './Toast.module.scss';
 
 const ToastUI = () => {
-  const { open, message, duration, closeToast, showCloseButton } = useToastStore();
+  const { open, type, message, duration, closeToast, showCloseButton } = useToastStore();
 
   return (
     <Toast.Provider swipeDirection="right" duration={duration}>
-      <Toast.Root className={styles.Root} open={open} onOpenChange={closeToast}>
+      <Toast.Root
+        className={`${styles.Root} ${styles[type]}`}
+        open={open}
+        onOpenChange={closeToast}
+      >
         <Toast.Title className={styles.Title}>{message}</Toast.Title>
         {showCloseButton && (
           <Toast.Close className={styles.close}>
