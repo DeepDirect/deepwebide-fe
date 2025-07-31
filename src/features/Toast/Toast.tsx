834ccd6c02275181b20011eb0f887b9ae18f0f79
Shin-Yu-1 <shingyu.user@gmail.com>
2025-07-31 10:08:45 +0900

    style: isDarkMode에 따라 토스트 배경색 달라지도록 수정 / DP-166

--- src/features/Toast/Toast.tsx
+++ src/features/Toast/Toast.tsx
@@ -1,14 +1,16 @@
 import * as Toast from '@radix-ui/react-toast';
+import { useThemeStore } from '@/stores/themeStore';
 import { useToastStore } from '@/stores/toastStore';
 import styles from './Toast.module.scss';
 
 const ToastUI = () => {
+  const { isDarkMode } = useThemeStore();
   const { open, type, message, duration, closeToast, showCloseButton } = useToastStore();
 
   return (
     <Toast.Provider swipeDirection="right" duration={duration}>
       <Toast.Root
-        className={`${styles.Root} ${styles[type]}`}
+        className={`${styles.Root} ${styles[type]} ${isDarkMode ? styles.dark : styles.light}`}
         open={open}
         onOpenChange={closeToast}
       >
