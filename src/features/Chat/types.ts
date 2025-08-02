77b5944b56073ea11f4b1077adb3903ca8fc17d4
eunlee <eklee2057@naver.com>
2025-08-02 19:47:12 +0900

    feat(Chat): 채팅 검색 결과에 대한 타입 새로 정의 / DP-155

--- src/features/Chat/types.ts
+++ src/features/Chat/types.ts
@@ -30,6 +30,22 @@ export interface ChatSendMessage {
   codeReference: SendCodeReference | null;
 }
 
+export interface SearchedChatMessageElement {
+  messageId: string;
+  senderId: number;
+  senderNickname: string;
+  senderProfileImageUrl: string;
+  message: string;
+  codeReference: CodeReference | null;
+  isMine: boolean;
+  sentAt: string;
+}
+export interface SearchedChatMessage {
+  keyword: string;
+  totalElements: number;
+  messages: SearchedChatMessageElement[];
+}
+
 export type ChatReceivedMessageType = ChatReceivedMessage;
 export type ChatSendMessageType = ChatSendMessage;
 export type ChatMessageInfoType = ChatMessageInfo;
